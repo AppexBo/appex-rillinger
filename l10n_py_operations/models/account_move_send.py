ec5789c26b5108213018edec486bbe5bef74e0d9
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2025-03-04 09:38:03 -0400

    Fix: Use BytesIO to generate PDF instead a physical file.

--- l10n_py_operations/models/account_move_send.py
+++ l10n_py_operations/models/account_move_send.py
@@ -2,25 +2,24 @@
 
 from odoo import api, models, fields
 import base64
+import io
 
 import logging
 _logger = logging.getLogger(__name__)
 
 
-
 class AccountMoveSend(models.TransientModel):
-    _inherit = ['account.move.send']    
+    _inherit = ['account.move.send']
 
-    
-    
     @api.model
     def _prepare_invoice_pdf_report(self, invoice, invoice_data):
         if invoice.invoice_pdf_report_id:
             return
 
-        #content, _report_format = self.env['ir.actions.report']._render(f'l10n_bo_bolivian_invoice.ir_actions_report_invoice_bo_{invoice[0].pos_id.paper_format_type}', invoice.ids)
         if invoice[0].company_id.use_auto_invoice:
-            content, _report_format = self.env['ir.actions.report']._render(f'l10n_py_operations.ir_actions_report_invoice_py_1', invoice.ids)
+            content, _report_format = self.env['ir.actions.report']._render(
+                'l10n_py_operations.ir_actions_report_invoice_py_1', invoice.ids
+            )
 
             invoice_data['pdf_attachment_values'] = {
                 'raw': content,
@@ -28,25 +27,23 @@ class AccountMoveSend(models.TransientModel):
                 'mimetype': 'application/pdf',
                 'res_model': invoice._name,
                 'res_id': invoice.id,
-                'res_field': 'invoice_pdf_report_file', # Binary field
+                'res_field': 'invoice_pdf_report_file',  # Binary field
             }
         else:
             content = invoice.get_request_pdf()
             if content:
                 pdf_binary_content = base64.b64decode(content)
-
-                with open("output.pdf", "wb") as pdf_file:
-                    pdf_file.write(pdf_binary_content)
+                # Usamos io.BytesIO en lugar de escribir en disco, esto dado que tenemos un error de escritura en el contenedor
+                # Siempre usar io.BytesIO para generar pdf!
+                pdf_buffer = io.BytesIO(pdf_binary_content)
 
                 invoice_data['pdf_attachment_values'] = {
-                    'raw': pdf_binary_content,
+                    'raw': pdf_buffer.getvalue(),
                     'name': invoice._get_invoice_report_filename(),
                     'mimetype': 'application/pdf',
                     'res_model': invoice._name,
                     'res_id': invoice.id,
-                    'res_field': 'invoice_pdf_report_file', # Binary field
+                    'res_field': 'invoice_pdf_report_file',
                 }
             else:
-                
                 super(AccountMoveSend, self)._prepare_invoice_pdf_report(invoice, invoice_data)
-                
